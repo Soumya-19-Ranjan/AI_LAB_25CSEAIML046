213a9b138a59d06d47175a76843e677eb4a04379
soumya <25cseaiml046.soumyaranjanpradhan@giet.edu>
2026-09-25 09:45:37 +0530

    Added Water Jug Program

--- Water_jug.py
+++ Water_jug.py
@@ -25,3 +25,18 @@ def water_jug(cap1, cap2, target_state):
             (j1 - min(j1, cap2 - j2), j2 + min(j1, cap2 - j2)),  # Pour jug1 to jug2
             (j1 + min(j2, cap1 - j1), j2 - min(j2, cap1 - j1))   # Pour jug2 to jug1
         ]
+        for move in next_moves:
+            if move not in visited:
+                queue.append((move[0], move[1], current_path))
+
+    return None  # No solution found
+
+target_goal = (4,2)
+solution = water_jug(4, 3, target_goal)
+
+if solution:
+    print(f"steps to reach the target state {target_goal}:")
+    for step in solution:
+        print(step)
+else:
+    print("No solution exists.")
